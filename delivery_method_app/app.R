e1a2e12400de4745146e7c615949030f98bb7545
smitsrr <smitsrr@gmail.com>
2017-12-29 15:56:46 -0700

    new data files, data exploration in shiny app

--- delivery_method_app/app.R
+++ delivery_method_app/app.R
@@ -15,17 +15,17 @@ setwd("..")
      #import some data
     # Note, you have to delete the comments at the end of the text files. 
 asian<- read.delim("wonder_data_extracts/Natality_2015_asian.txt", 
-                             sep = "\t", header = TRUE, colClasses = "character")
+                             sep = "/t", header = TRUE, colClasses = "character")
 american_indian<- read.delim("wonder_data_extracts/Natality_2015_american_indian.txt", 
-                             sep = "\t", header = TRUE, colClasses = "character")
+                             sep = "/t", header = TRUE, colClasses = "character")
 black<- read.delim("wonder_data_extracts/Natality_2015_black.txt", 
-                   sep = "\t", header = TRUE, colClasses = "character")
+                   sep = "/t", header = TRUE, colClasses = "character")
 white_1<- read.delim("wonder_data_extracts/Natality_2015_white_1.txt", 
-                     sep = "\t", header = TRUE, colClasses = "character")
+                     sep = "/t", header = TRUE, colClasses = "character")
 white_2<- read.delim("wonder_data_extracts/Natality_2015_white_2.txt", 
-                     sep = "\t", header = TRUE, colClasses = "character")
+                     sep = "/t", header = TRUE, colClasses = "character")
 white_3<- read.delim("wonder_data_extracts/Natality_2015_white_3.txt", 
-                     sep = "\t", header = TRUE, colClasses = "character")
+                     sep = "/t", header = TRUE, colClasses = "character")
     # append all of the data
 natality<- rbind(asian
                  ,american_indian
@@ -34,6 +34,45 @@ natality<- rbind(asian
                  ,white_2
                  ,white_3) %>%
   select(-Notes)
+
+#####################
+# Race and medical risk factors
+#####################
+setwd("C:/Users/smits/Documents/GitHub/delivery_method/wonder_data_extracts/race_risk_factors")
+
+wh_diabetes<- read.delim("Natality_2015_white_diabetes_v2.txt",
+                          header = TRUE, colClasses = "character") %>%
+  mutate(risk_factor = ifelse(Diabetes == "Yes", "diabetes", "none"))%>%
+  select(-Diabetes, -Diabetes.Code)
+wh_eclampsia<- read.delim("Natality_2015_white_eclampsia_v2.txt",
+                          header = TRUE, colClasses = "character")%>%
+  mutate(risk_factor = ifelse(Eclampsia == "Yes", "eclampsia", "none"))%>%
+  select(-Eclampsia, -Eclampsia.Code)
+wh_hypertension<- read.delim("Natality_2015_white_hypertension_v2.txt",
+                          header = TRUE, colClasses = "character") %>%
+  mutate(risk_factor = ifelse(Chronic.Hypertension == "Yes", "chronic.hypertension", "none"))%>%
+  select(-Chronic.Hypertension, -Chronic.Hypertension.Code)
+wh_preg_hypertension<- read.delim("Natality_2015_white_pregnancy_hypertension_v2.txt",
+                          header = TRUE, colClasses = "character")%>%
+  mutate(risk_factor = ifelse(Pregnancy.associated.Hypertension == "Yes", "preg_hypertension", "none"))%>%
+  select(-Pregnancy.associated.Hypertension, -Pregnancy.associated.Hypertension.Code )
+wh_tobacco<- read.delim("Natality_2015_white_tobacco_v2.txt",
+                        header = TRUE, colClasses = "character")%>%
+  mutate(risk_factor = ifelse(Tobacco.Use.Code == "Yes", "tobacco", "none")) %>%
+  select(-Tobacco.Use.Code, -Tobacco.Use)
+
+# append all of the data
+natality<- rbind(wh_diabetes
+                 ,wh_eclampsia
+                 ,wh_hypertension
+                 ,wh_preg_hypertension
+                 ,wh_tobacco) %>%
+  mutate(Births = as.numeric(Births))
+
+county_births_check <- natality %>%
+  group_by(County,County.Code, Delivery.Method) %>%
+  summarize(births = sum(Births))
+
 
     # county and FIPS codes from https://www.census.gov/geo/reference/codes/cou.html
 counties<- read.delim("national_county.txt", header = FALSE, sep = ",",
@@ -75,6 +114,12 @@ ggplot(map.df, aes(x=long, y=lat, group=group, fill=obesity)) +
   geom_polygon()+coord_map()+
   labs(title="2010 Adult Obesity by Country, percent",x="",y="")+
   theme_bw()
+
+    # NOTES
+# choose age of mother
+# [extension, choose medical risk factor]
+# Choose month prenatal care began
+# display US map of c-section rate (using bi-directional color scale)
 
 
 # Define UI for application that draws a histogram
