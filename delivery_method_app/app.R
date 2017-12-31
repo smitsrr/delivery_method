54e50636601ee734211167214149e3b2ebaaff31
smitsrr <smitsrr@gmail.com>
2017-12-31 09:26:31 -0700

    tooltips started, nearpoint is off though

--- delivery_method_app/app.R
+++ delivery_method_app/app.R
@@ -21,24 +21,11 @@ setwd("C:/Users/smits/Documents/GitHub/delivery_method")
     # you include segments. 
 
     # age and race - use this for the pop-up next to the map. 
-# natality<- read.delim("wonder_data_extracts/Natality_12_15_race_age_v2.txt", 
-#                    header = TRUE, colClasses = "character")
-
-
-
-    #total county rates - first born children
-natality_counties<- read.delim("wonder_data_extracts/Natality_12_15_county_totals_v2.txt", 
-                      header = TRUE, colClasses = "character") %>%
-  filter(Births != "Suppressed") %>%
-  mutate(Births = as.numeric(Births)) %>%
-  group_by(County.Code, County) %>%
-  mutate(cesarean_rate_first = round(Births/sum(Births),4)*100,
-         births_first = sum(Births)) %>%
-  filter(Delivery.Method == "Cesarean") %>%
-  select(-Notes, -Births, -Delivery.Method, -Delivery.Method.Code)
+natality_race_age<- read.delim("wonder_data_extracts/Natality_12_15_race_age_v2.txt",
+                  header = TRUE, colClasses = "character")
 
     # county totals, all births
-natality_counties_all<- read.delim("wonder_data_extracts/Natality_12_15_county_totals_all_births_v2.txt", 
+natality<- read.delim("wonder_data_extracts/Natality_12_15_county_totals_all_births_v2.txt", 
                                header = TRUE, colClasses = "character") %>%
   filter(Births != "Suppressed") %>%
   mutate(Births = as.numeric(Births)) %>%
@@ -47,9 +34,6 @@ natality_counties_all<- read.delim("wonder_data_extracts/Natality_12_15_county_t
          births_all = sum(Births)) %>%
   filter(Delivery.Method == "Cesarean") %>%
   select(-Notes, -Births, -Delivery.Method, -Delivery.Method.Code)
-
-    # join the counts/rates for first births v. all births
-natality<- left_join(natality_counties, natality_counties_all)
 
     # population totals for counties
     # https://www.census.gov/data/datasets/2016/demo/popest/counties-total.html#ds
@@ -105,7 +89,13 @@ ui <- fluidPage(
    # Application title
    titlePanel("Cesarean section rate"),
    
-  plotOutput("county_map")
+   div(
+     style = "position:relative",
+     plotOutput("county_map", 
+                hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce")),
+     uiOutput("hover_info")
+   )
+
 )
 
 # Define server logic required to draw a histogram
@@ -116,26 +106,61 @@ server <- function(input, output) {
       p<- ggplot(map.df, aes(x=long, y=lat, group = group)) +
         geom_polygon( colour = "grey" , aes( fill = cesarean_rate_all.x )) +
         scale_fill_gradientn("",colours=brewer.pal(9,"YlGnBu"))+
-        coord_map("polyconic" ) +
-        geom_path(data = state_map, colour="black")+
-        theme_void()
+        coord_map() +
+        # coord_map("polyconic" ) + #causes the tooltips to not work as well
+        geom_path(data = state_map, colour="black")
+      # +
+      #   theme_void()
 
       # unfilled is unavailable due to population changes. 
       # grey/NA is an 'unidentified' county
     p
    })
+  
+  output$hover_info<-renderUI({
+    hover <- input$plot_hover
+    point <- nearPoints(map.df, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+    if (nrow(point) == 0) return(NULL)
+    
+    # calculate point position INSIDE the image as percent of total dimensions
+    # from left (horizontal) and from top (vertical)
+    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+    
+    # calculate distance from left and bottom side of the picture in pixels
+    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+    
+    # create style property fot tooltip
+    # background color is set so tooltip is a bit transparent
+    # z-index is set so we are sure are tooltip will be on top
+    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+                    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+    
+    # actual tooltip created as wellPanel
+    wellPanel(
+      style = style,
+      p(HTML(paste0("<b></b>", point$County_all_pop, "<br/>",
+                    "<b> Region Population</b>", point$population_all_pop, "<br/>",
+                    "<b> County Population: </b>", point$population, "<br/>",
+                    "<b> Region Cesarean Rate: </b>", point$cesarean_rate_all_pop, "<br/>",
+                    "<b> County Cesarean Rate: </b>", point$cesarean_rate_all.x, "<br/>",
+                    "<b> Distance from left: </b>", left_pct, "/", left_px, 
+                    "<b>, from top: </b>", top_pct, "/", top_px)))
+    )
+  })
+  
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+runApp(list(ui = ui, server = server))
 
 ## TO DO
-# add state lines
-# add faint lines between counties. 
+# tooltips to ggmap/plot object - 'nearpoint' is definitely OFF
 # click on a county, get rates by ethnicity/age
 # age of mom = color of point, x-axis = race, y-axis = cesarean rate
 # if age is too cluttered, we could facet by race (there are only 4 )
-# resize the map - figure out where the other graphs are going to go
+# Since i'm not going to use plotly, think about having a zoom feature for states? 
 
 ## NOTES
 # I don't know if I'm a huge fan of having the county name for unidentified counties. 
@@ -143,16 +168,6 @@ shinyApp(ui = ui, server = server)
 
 # after comparing, it's pretty clear that first birth rates are not different from all
 # I think I could just present all. 
-
-
-# NOTES
-# choose age of mother
-# [extension, choose medical risk factor]
-# Choose month prenatal care began
-# display US map of c-section rate (using bi-directional color scale)
-# compare only first-births to 'all births'
-# show/remove hospital layer. 
-
 
 
 # plot hospital layer?
